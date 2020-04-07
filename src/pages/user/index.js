c0ad34aa422849a9daded0f6a66755a10a79e118
clevok <1029794653@qq.com>
2020-04-07 16:00:34 +0800

    update

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,41 +1,50 @@
 let event = require('../../core/event');
-const {router} = require('../../router');
+const { router } = require('../../router');
 
 Page.P(router.pages.user.index, {
-    data: {
-        tabs: [
-            {value: 0, text: '我的场所'}, 
-            {value: 1, text: '我的攻略'}, 
-            {value: 2, text: '消息'}
-        ]
-    },
-    onAppLaunch: function (opts) {
-        console.log('[pages/index]  程序启动：', opts)
-    },
-    onLoad: function() {
-        this.$preload('user?cid=123');
-    },
-    onReady: function () {
+	data: {
+		tabs: [
+			{ value: 0, text: '我的场所' },
+			{ value: 1, text: '我的攻略' },
+			{ value: 2, text: '消息' }
+		]
+	},
 
-    },
-    onPlay: function () {
-        event.emit('change');
-    },
-    redirect () {
-        this.$redirect('/pages/user/user')
-    },
-    route () {
-        wx.navigateTo({
-            url: '../place/place'
-        })
-    },
-    navigate() {
-        this.$navigate('user?cid=abcd')
-    },
-    onNavigate (taget) {
-        console.log('监听页面跳转 onNavigate: taget', taget)
-    },
-    onShow: function () {
-        console.log('[pages/index] 页面展示')
-    }
+	/** 这里写预加载的方法 */
+	onPreload() {
+		this.$set('api/reuest', Api.demo({
+			demo: 2
+		}))
+	},
+	onLoad: function () {
+		const data = await this.$take('user?cid=123') || Api.demo({
+			demo: 2
+		});
+
+	},
+
+
+	onReady: function () {
+
+	},
+	onPlay: function () {
+		event.emit('change');
+	},
+	redirect() {
+		this.$redirect('/pages/user/user')
+	},
+	route() {
+		wx.navigateTo({
+			url: '../place/place'
+		})
+	},
+	navigate() {
+		this.$navigate('user?cid=abcd')
+	},
+	onNavigate(taget) {
+		console.log('监听页面跳转 onNavigate: taget', taget)
+	},
+	onShow: function () {
+		console.log('[pages/index] 页面展示')
+	}
 })
